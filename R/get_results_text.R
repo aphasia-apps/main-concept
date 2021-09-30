292aede3870d4ba217e1a884529d5ac80f95fed2
Rob Cavanaugh <rob.cavanaugh@pitt.edu>
2021-09-29 21:12:06 -0400

    glue prefix

--- R/get_results_text.R
+++ R/get_results_text.R
@@ -7,14 +7,13 @@
 #' @export
 
 get_results_text <- function(results, num_concepts, time){
-  library(glue)
-  txt_acc <- glue('
+  txt_acc <- glue::glue('
        
     The main concept score for this person is {results[1,3]}. This is in the {results[1,4]} percentile of PWAs and the {results[1,5]} percentile of controls. This individual produced {results[2,2]}/{num_concepts} accurate/complete main concepts, which is in the {results[2,4]} percentile of PWAs and the {results[2,5]} percentile of controls. This individual produced {results[3,2]}/{num_concepts} accurate/incomplete main concepts, {results[4,2]}/{num_concepts} inaccurate/complete main concepts, and {results[5,2]}/{num_concepts} inaccurate/incomplete main concepts. There were {results[7,2]}/{num_concepts} main concept attempts, which is in the {results[7,4]} percentile of PWAs and {results[7,5]} percentile of controls. Finally, {results[6,2]}/{num_concepts} main concepts were absent in this individual’s sample.  
     ')
   
   if(time > 0){ 
-  txt_eff <- glue('
+  txt_eff <- glue::glue('
     This individual produced {results[8,2]} main concepts/minute, which is in the {results[8,4]} percentile of PWAs and the {results[8,5]} percentile of controls.  
                   ')
   
